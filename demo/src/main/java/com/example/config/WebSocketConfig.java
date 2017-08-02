a3d95fb3dee21c0674991042f5335ba5e3e48d1a
shengluoto <914001985@qq.com>
2017-08-02 21:46:50 +0800

    scala配置

---
+++ demo/src/main/java/com/example/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
