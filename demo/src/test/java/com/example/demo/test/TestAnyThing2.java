67ba3ae8982ebb6938f528b87217c58bd770d302
shengluoto <914001985@qq.com>
2018-10-26 11:09:20 +0800

    gx

--- demo/src/test/java/com/example/demo/test/TestAnyThing2.java
+++ demo/src/test/java/com/example/demo/test/TestAnyThing2.java
@@ -7,34 +7,40 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.junit.Test;
 
 public class TestAnyThing2 {
-	@Test
-	public void test13(){
-//		String host = "0.tcp.ngrok.io"; //		int port = 10666; 		
-		String host = "10.10.58.242"; 		
-		int port = 8082; 		
-		try { 			
-			while(true) { 				
-				Socket client = new Socket(host, port); 				
-				OutputStream outToServer = client.getOutputStream(); 				
-				DataOutputStream out = new DataOutputStream(outToServer); 				 				
-				out.writeUTF(">>cssp,0,SN0301201809180001,0,heartb,none,1,0,05\n"); 				
-				InputStream inFromServer = client.getInputStream(); 				
-				DataInputStream in = new DataInputStream(inFromServer); 				
-				System.out.println("服务器响应： " + in.readUTF()); 				
-				client.close(); 				
-				Thread.sleep(1000*300); 			
-				}	 		
-		} catch (UnknownHostException e) { 			
-			e.printStackTrace(); 		
-		} catch (IOException e) { 			
-			e.printStackTrace(); 		
-		} catch (InterruptedException e) { 			
-			// TODO Auto-generated catch block 			
-			e.printStackTrace(); 		
-		}
+    @Test
+    public void test13() {
+	String host = "0.tcp.ngrok.io";
+	int port = 10666;
+	String msg = ">>cssp,0,SN0301201809180001,0,heartb,none,1,0,05\n";
+	// String host = "10.10.58.242";
+	// int port = 8082;
+	try {
+	    while(true) {
+		System.out.println(LocalDate.now().toString() + LocalTime.now().toString() + " 开始发送： ");
+		Socket client = new Socket(host, port);
+		OutputStream outToServer = client.getOutputStream();
+		DataOutputStream out = new DataOutputStream(outToServer);
+		byte[] bstream = msg.getBytes("GBK");
+		out.write(bstream);
+		InputStream inFromServer = client.getInputStream();
+		DataInputStream in = new DataInputStream(inFromServer);
+		System.out.println(LocalDate.now().toString() + LocalTime.now().toString() + "服务器响应： " + in.read());
+		client.close();
+		Thread.sleep(1000 * 300);
+	    }
+	}catch (UnknownHostException e) {
+	    e.printStackTrace();
+	}catch (IOException e) {
+	    e.printStackTrace();
+	}catch (InterruptedException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 }
